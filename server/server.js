cfeb494cc26b74d74232863c33d1d00d3141ae4c
Srushti Awadhiya <.>
2026-09-01 14:41:40 +0530

    updates

--- server/server.js
+++ server/server.js
@@ -1,23 +1,28 @@
 import express from 'express';
 import cors from 'cors';
-import 'dotenv/config'
+import 'dotenv/config';
 
 import connectDB from './config/mongodb.js';
 import userRouter from './Route/userRoute.js';
 import imageRouter from './Route/ImageRoutes.js';
 
-const PORT = process.env.PORT || 4000
-const app = express()
+const app = express();
 
+app.use(cors({
+    origin: 'https://vision-craft-yjlb.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'token']
+}));
 
-
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
 
 await connectDB();
 
-app.use('/api/user',userRouter)
-app.use('/api/image',imageRouter)
-app.get('/',(req,res)=> res.send("API Working fine"))
+app.use('/api/user', userRouter);
+app.use('/api/image', imageRouter);
 
-app.listen(PORT,()=> console.log('Server running on port ' + PORT));
+app.get('/', (req, res) => {
+    res.send('API Working fine');
+});
+
+export default app;
